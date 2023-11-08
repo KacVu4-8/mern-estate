6b5e0f5872cdd69e249d682bd0a463302e68c79b
KacVu4-8 <khacvu908@gmail.com>
2023-11-08 15:46:22 +0700

    sign up page funcionaly

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,7 +8,8 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully");
-  } catch (error) {
+  
+    } catch (error) {
     next(error);
   }
 };
