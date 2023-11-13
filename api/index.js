1546a2f818527eb35cf9482ef79633ba8d034e11
KacVu4-8 <khacvu908@gmail.com>
2023-11-13 22:04:40 +0700

    final

--- api/index.js
+++ api/index.js
@@ -1,19 +1,17 @@
-import path from "path";
-import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-import exp from "constants";
-
+import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
@@ -28,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("listening on 3000");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
